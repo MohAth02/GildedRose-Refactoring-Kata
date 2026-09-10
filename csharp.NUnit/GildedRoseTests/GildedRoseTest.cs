240d3461fcbdae262d5a062fdf5487989e3e2028
MohAth02 <mohamed.athamena@softwire.com>
2026-09-10 09:22:49 +0100

    initial tests for aged brie and sulfuras

--- csharp.NUnit/GildedRoseTests/GildedRoseTest.cs
+++ csharp.NUnit/GildedRoseTests/GildedRoseTest.cs
@@ -12,6 +12,61 @@ public class GildedRoseTest
         var items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
         var app = new GildedRose(items);
         app.UpdateQuality();
-        Assert.That(items[0].Name, Is.EqualTo("fixme"));
+        Assert.That(items[0].Name, Is.EqualTo("foo"));
+    }
+
+    [Test]
+
+    public void testAgedBrieIncrease()
+    {
+        var items = new List<Item> { new Item { Name = "Aged Brie", SellIn = 1000, Quality = 0 } };
+        var app = new GildedRose(items);
+        for (var i = 0; i < 5; i++)
+        {
+            app.UpdateQuality();
+        }
+
+        Assert.That(items[0].Quality, Is.EqualTo(5));
+    }
+
+
+    [Test]
+    public void testAgedBrieIncreaseByTwoafterSellInExpires()
+    {
+        var items = new List<Item> { new Item { Name = "Aged Brie", SellIn = 0, Quality = 0 } };
+        var app = new GildedRose(items);
+        for (var i = 0; i < 5; i++)
+        {
+            app.UpdateQuality();
+        }
+
+        Assert.That(items[0].Quality, Is.EqualTo(10));
+    }
+
+    [Test]
+    public void testSulfurasQualityStayAt80()
+    {
+        var items = new List<Item> { new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80 } };
+        var app = new GildedRose(items);
+        for (var i = 0; i <= 15; i++)
+        {
+            app.UpdateQuality();
+        }
+
+        Assert.That(items[0].Quality, Is.EqualTo(80));
+    }
+
+
+    [Test]
+    public void testBackstagePassesSellingStaysTheSame()
+    {
+        var items = new List<Item> { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 15, Quality = 20 } };
+        var app = new GildedRose(items);
+        for (var i = 0; i <= 15; i++)
+        {
+            app.UpdateQuality();
+        }
+
+        Assert.That(items[0].SellIn, Is.EqualTo(15));
     }
 }
